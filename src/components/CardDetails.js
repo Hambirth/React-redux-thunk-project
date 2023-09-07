9b18e1c45c9bf4ba793f4137715b529411199e17
Hambirth <102298849+Hambirth@users.noreply.github.com>
2023-09-07 14:54:18 +0530

    React-redux-thunk

---
+++ src/components/CardDetails.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./cartstyle.css";
+
+const CardDetails = () => {
+  return (
+    <div>
+      <h1>Card Details</h1>
+    </div>
+  );
+};
+
+export default CardDetails;
---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+
+const Header = () => {
+  return (
+    <>
+      <Navbar
+        style={{ height: "60px", backgroundColor: "black", color: "white" }}
+      >
+        <Container>
+          <h3 className="text-light">Ecommerce Hazaribagh</h3>
+
+          <div id="ex4">
+            <span className="p1 fa-stack fa-2x has-badge" data-count={1}>
+              <i class="fa-solid fa-cart-shopping"></i>
+            </span>
+          </div>
+        </Container>
+      </Navbar>
+    </>
+  );
+};
+
+export default Header;
